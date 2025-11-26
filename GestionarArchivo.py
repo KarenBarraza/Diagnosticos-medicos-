b88565a683c68188ea53891ee38d551a01ca52b0
santiago avila quintero <SAQ>
2025-11-26 08:18:08 -0500

    feat:(GestionarArchivo) creacion la clase GestionarArchivo y la funcion leer_datos

---
+++ GestionarArchivo.py
@@ -0,0 +1,16 @@
+import csv
+from csv import DictWriter
+
+
+class GestionarArchivo:
+    def __init__(self):
+        pass
+
+    def leer_datos(self, ruta):
+        filas = []
+        with open(ruta, encoding="utf-8") as f:
+            lector = csv.DictReader(f)
+            for fila in lector:
+                filas.append(fila)
+        return filas
+
