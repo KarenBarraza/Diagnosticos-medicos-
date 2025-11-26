04955ec365bea9b05314789462039ca583ee1c16
santiago avila quintero <SAQ>
2025-11-26 11:27:17 -0500

    feat: creacion de de archivos de prueva

---
+++ NormalizacionDatos.py
@@ -0,0 +1,17 @@
+
+from datetime import datetime
+
+class NormalizacionDatos:
+    def __init__(self):
+        pass
+    
+    def MostrarListado(self):
+        archivo=()
+        datos=archivo.leer()#no esta reminado
+        for lista in datos:
+            print(lista)
+
+
+
+m=NormalizacionDatos()
+m.MostrarListado()     
