c6005dae2d087cb447f6866994ae1a2b3b371ac1
Karen Gisell Rodriguez Barraza <barrazak940>
2025-11-27 10:20:11 -0500

    feat:se creo la funcion para normalizar el formato de fecha y se llamo la funcion en ProcesamientoDatos.py

--- NormalizacionDatos.py
+++ NormalizacionDatos.py
@@ -1,5 +1,22 @@
-from GestionarArchivo import csv
-from datetime import datetime
+from GestionarArchivo import GestionarArchivo
+import datetime
 
-class ConvertirFecha(self, admission_date, discharge_date):
+class NormalizacionDatos:
+    def _init_(self):
+        pass
 
+    formatos_entrada = ["%d/%m/%Y", "%Y-%m-%d", "%d-%m-%Y", "%m/%d/%Y"]
+
+    def convertirFecha(self, fecha, formato_salida):
+        for f in self.formatos_entrada:
+            try:
+                return datetime.datetime.strptime(fecha, f).strftime(formato_salida)
+            except ValueError:
+                continue
+        return "0000-00-00"
+
+
+
+   
+
+    
---
+++ ProcesamientoDatos.py
@@ -0,0 +1,14 @@
+from GestionarArchivo import GestionarArchivo
+from NormalizacionDatos import NormalizacionDatos
+
+gestor = GestionarArchivo()
+datos = gestor.leer_datos("dataset5_patient_diagnoses.csv")
+
+for dato in datos:
+    normalizar = NormalizacionDatos()
+    dato["admission_date"] = normalizar.convertirFecha(dato["admission_date"], "%Y-%m-%d")
+    dato["discharge_date"] = normalizar.convertirFecha(dato["discharge_date"], "%Y-%m-%d")
+
+for i in datos:
+    print(datos)
+
