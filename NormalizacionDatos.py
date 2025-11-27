e95ea2f590b461b5e1d89dc42083cd9320d55e15
Karen Gisell Rodriguez Barraza <barrazak940>
2025-11-27 11:53:08 -0500

    feat: se crearon dos funciones para calcula la fecha de los espacios vacios en el archivo principal

--- NormalizacionDatos.py
+++ NormalizacionDatos.py
@@ -1,9 +1,8 @@
 from GestionarArchivo import GestionarArchivo
 from datetime import datetime
-from datetime import timedelta
 
 class NormalizacionDatos:
-    def _init_(self):
+    def __init__(self):
         pass
 
     formatos_entrada = ["%d/%m/%Y", "%Y-%m-%d", "%d-%m-%Y", "%m/%d/%Y"]
@@ -11,26 +10,32 @@ class NormalizacionDatos:
     def convertirFecha(self, fecha, formato_salida):
         for f in self.formatos_entrada:
             try:
-                return datetime.datetime.strptime(fecha, f).strftime(formato_salida)
+                return datetime.strptime(fecha, f).strftime(formato_salida)
             except ValueError:
                 continue
-        return "0000-00-00"
-    
+            return "0000-00-00"
+        
 
-    # def calcular_fecha_salida(fecha_ingreso, dias_hospitalizacion):
-    #     fecha_ingreso = datetime.datetime.strptime(fecha_ingreso, "%Y-%m-%d")
-    #     fecha_salida = fecha_ingreso + datetime.timedelta(days=dias_hospitalizacion)
-    #     return fecha_salida.strftime("%Y-%m-%d")
+    def calcular_fecha_salida(self, fecha_ingreso, dias_hospitalizacion):
+        fecha_ingreso = datetime.datetime.strptime(fecha_ingreso, "%Y-%m-%d")
+        fecha_salida = fecha_ingreso + datetime.timedelta(days=dias_hospitalizacion)
+        return fecha_salida.strftime("%Y-%m-%d")
 
-    # for dato in datos:
-    #     if dato["discharge_date"] == "":
-    #         dato["discharge_date"] = calcular_fecha_salida(dato["admission_date"], dato["length_of_stay_days"])
+
+    def procesar_datos(self):
+        for dato in self.datos:
+            dato["admission_date"] = self.normalizar.convertirFecha(dato["admission_date"], "%Y-%m-%d")
+            if dato["discharge_date"] in dato and dato["discharge_date"] == " ":
+                dias_hospitalizacion = int(dato.get("length_of_stay_days", 1) or 1)
+                dato["discharge_date"] = self.calcular_fecha_salida(dato["admission_date"], dias_hospitalizacion)
+
 
     def MostrarListado(self,datos):
         for lista in datos:
             print(lista)
 
-    def RemplasarEspaciosBacios(self,datos):
+
+    def ReemplazarEspaciosVacios(self,datos):
         for lista in datos:
             if lista['length_of_stay_days'] == "":
                 lista['length_of_stay_days']=0
--- ProcesamientoDatos.py
+++ ProcesamientoDatos.py
@@ -1,16 +1,18 @@
 from GestionarArchivo import GestionarArchivo
 from NormalizacionDatos import NormalizacionDatos
-
+1
 gestor = GestionarArchivo()
 datos = gestor.leer_datos("dataset5_patient_diagnoses.csv")
 normalizar = NormalizacionDatos()
-normalizar.RemplasarEspaciosBacios(datos)
+normalizar.ReemplazarEspaciosVacios(datos)
+
 for dato in datos:
     dato["admission_date"] = normalizar.convertirFecha(dato["admission_date"], "%Y-%m-%d")
     dato["discharge_date"] = normalizar.convertirFecha(dato["discharge_date"], "%Y-%m-%d")
 
+
 while True:
-    print("Menu \n 1. ver lista \n 2. no dificar espacion en blanco LOSD")
+    print("Menu \n 1. ver lista \n 2. Modificar espacion en blanco LOSD")
     opcion=input()
 
     if opcion=="1":
