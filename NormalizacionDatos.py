6b90b76e6f6c2158a6e0d041b5237167d632b027
Michelle Adriana Mogollon Uriza <Mich>
2025-11-28 15:41:48 -0500

    chore:Se hace comentarios para explicar cada linea correspondiente del codigo

--- NormalizacionDatos.py
+++ NormalizacionDatos.py
@@ -1,22 +1,25 @@
+#Se importa la clase necesaria para trabajar
 from GestionarArchivo import GestionarArchivo
+#Se importa una libreria con el formato de fecha
 from datetime import datetime , timedelta
 
+#Se crea una clase que tendra las funciones necesarias para normalizar los datos
 class NormalizacionDatos:
     def __init__(self):
         pass
-            
+#Funcion para corregir la escritura (todo creara escrito con mayusucula iniciañ y minusuculas)           
     def normalizar_texto(self, texto):
         if not texto:
             return ""
-        # Convierte todo a minúscula y luego la primera letra a mayúscula
+#Convierte todo a minúscula y luego la primera letra a mayúscula
         return texto.lower().capitalize()
-
+#Limpia las filas "diaggnosis" y "diagnosis group"
     def limpiar_diagnosticos(self,datos):
         for fila in datos:
             fila["diagnosis"] = self.normalizar_texto(fila.get("diagnosis", ""))
             fila["diagnosis_group"] = self.normalizar_texto(fila.get("diagnosis_group", ""))
 
-
+#Normaliza las palabras "SI" y "NO" para que queden escritas de manera correcta
     def normalizar_valor(self, valor):
         datos = valor.strip().lower()
         if datos in ("si", "SÃ­"):
@@ -25,7 +28,7 @@ class NormalizacionDatos:
             return "NO"
         else:
             return datos
-
+#Inicia un contador de "id" desde 1001 hasta el ultimo valor que encuentre 
     def renumerar_ids(self, filas):
         contador = 1001
         for fila in filas:
@@ -33,18 +36,19 @@ class NormalizacionDatos:
             contador += 1
         return filas
 
-
+#Convierte todas las fechas del archivo a un solo formato de fecha (Y/m/d)
     formatos_entrada = ["%d/%m/%Y", "%Y-%m-%d", "%d-%m-%Y", "%m/%d/%Y"]
 
     def convertirFecha(self, fecha, formato_salida):
         for f in self.formatos_entrada:
             try:
                 return datetime.strptime(fecha, f).strftime(formato_salida)
+#Si encuentra un error retorna "0000-00-00"
             except ValueError:
                 continue
         return "0000-00-00"
         
-
+#Calcula la fecha de salida con la fecha de ingreso y los dias de estadia en el hospital
     def calcular_fecha_salida(self,datos):
         for lista in datos:
             if lista["discharge_date"]=='0000-00-00':
@@ -55,20 +59,20 @@ class NormalizacionDatos:
                     salidaF=ingresoF+ timedelta(days=lista['length_of_stay_days'])
                     lista["discharge_date"] = salidaF.strftime("%Y-%m-%d")
 
+#No se utiliza
+    #def procesar_datos(self):
+        #for dato in self.datos:
+            #dato["admission_date"] = self.normalizar.convertirFecha(dato["admission_date"], "%Y-%m-%d")
+            #if dato["discharge_date"] in dato and dato["discharge_date"] == " ":
+                #dias_hospitalizacion = int(dato.get("length_of_stay_days", 1) or 1)
+                #dato["discharge_date"] = self.calcular_fecha_salida(dato["admission_date"], dias_hospitalizacion)
 
-    def procesar_datos(self):
-        for dato in self.datos:
-            dato["admission_date"] = self.normalizar.convertirFecha(dato["admission_date"], "%Y-%m-%d")
-            if dato["discharge_date"] in dato and dato["discharge_date"] == " ":
-                dias_hospitalizacion = int(dato.get("length_of_stay_days", 1) or 1)
-                dato["discharge_date"] = self.calcular_fecha_salida(dato["admission_date"], dias_hospitalizacion)
-
-
+#Funcion para mostrar toda la lista
     def MostrarListado(self,datos):
         for lista in datos:
             print(lista)
 
-
+#Remplaza los vacios o N/A con 0 de la columna de estadia
     def ReemplazarEspaciosVacios(self,datos):
         for lista in datos:
             if lista['length_of_stay_days'] == "":
@@ -78,7 +82,7 @@ class NormalizacionDatos:
             else:
                 lista['length_of_stay_days']=int(lista['length_of_stay_days'])
 
-    
+ #Se remplazan los dias para crear logica con las dos fechas (Ingreso y salida)  
     def ModificacionDias(self,datos):
         for lista in datos:
             if isinstance(lista["admission_date"], str):
@@ -94,7 +98,7 @@ class NormalizacionDatos:
                 dias=1
             lista['length_of_stay_days']=dias
                 
-
+#Crea el archivo y lo imprime
     def CrearArchivo(self,datos):
         gestor=GestionarArchivo()
         Nombre_colupnas=[
